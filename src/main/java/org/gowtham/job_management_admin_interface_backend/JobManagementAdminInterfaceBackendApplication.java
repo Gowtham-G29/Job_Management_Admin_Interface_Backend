66fc444f85b73796ea29f7b148c70c6a4aaf9f67
Gowtham G <gowthamguruvayurappan29@gmail.com>
2025-09-13 10:21:10 +0530

    Initial commit

---
+++ src/main/java/org/gowtham/job_management_admin_interface_backend/JobManagementAdminInterfaceBackendApplication.java
@@ -0,0 +1,13 @@
+package org.gowtham.job_management_admin_interface_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagementAdminInterfaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobManagementAdminInterfaceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gowtham/job_management_admin_interface_backend/JobManagementAdminInterfaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gowtham.job_management_admin_interface_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobManagementAdminInterfaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
