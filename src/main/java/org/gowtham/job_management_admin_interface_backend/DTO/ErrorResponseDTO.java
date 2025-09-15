5c12eadf9cdf313c66fbb3bb0ee6c6e6ff414e32
Gowtham G <gowthamguruvayurappan29@gmail.com>
2025-09-15 09:21:10 +0530

    Created a ErrorResponseDTO class for error handling

---
+++ src/main/java/org/gowtham/job_management_admin_interface_backend/DTO/ErrorResponseDTO.java
@@ -0,0 +1,15 @@
+package org.gowtham.job_management_admin_interface_backend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDTO {
+
+    String message;
+    HttpStatus statusCode;
+}
